87b24a83143e0c74ff18f5565e10e759f4441cf8
Felipexvv <felipexvv@gmail.com>
2024-07-23 17:12:24 -0300

    Adicionando segunda atividade springBoot

---
+++ Santander-BootCamp-Java-Dio/src/test/java/me/dio/Santander_BootCamp_Java_Dio/SantanderBootCampJavaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_BootCamp_Java_Dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootCampJavaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
